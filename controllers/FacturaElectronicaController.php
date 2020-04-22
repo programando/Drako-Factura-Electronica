aed2ec29a54c4c983766862174b312df71ba2422
programando <jhonjamesmg@hotmail.com>
2020-04-22 11:35:26 -0500

    Update FacturaElectronicaController.php

--- controllers/FacturaElectronicaController.php
+++ controllers/FacturaElectronicaController.php
@@ -500,7 +500,7 @@ class FacturaElectronicaController extends Controller
     $stringXml = $this->xml->outputMemory(true);
     $this->xml->flush();
     unset(  $this->xml );
-    //file_put_contents( $this->nombreDocumento  , $stringXml);
+    file_put_contents( $this->nombreDocumento  , $stringXml);
   }
   
   
